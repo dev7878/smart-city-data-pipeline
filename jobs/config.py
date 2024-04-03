2f8fc58bb7e0bbed5e95b341175cffadc536f497
Dev Patel <devpatel5578@gmail.com>
2024-04-03 13:11:33 -0400

    project completed

--- jobs/config.py
+++ jobs/config.py
@@ -1,4 +1,4 @@
-configuration = {
-    "AWS_ACCESS_KEY": "your_aws_access_key",
-    "AWS_SECRET_KEY": "your_aws_secret_key",
-}
+# configuration = {
+#     "AWS_ACCESS_KEY": "your_aws_access_key",
+#     "AWS_SECRET_KEY": "your_aws_secret_key",
+# }
